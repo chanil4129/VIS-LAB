ab0dfeb841cf14ac039fd723afc7bf0ff103f1de
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-07-10 22:48:48 +0900

    UI(autocomplete,toolbar)

--- study/src/app/app.module.ts
+++ study/src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntersectionComponent } from './intersection/intersection.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from'@angular/material/autocomplete';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntersectionComponent,
+    AutocompleteComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
