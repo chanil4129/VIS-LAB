51e186fe7734225befe9a79ea7cc9f2f53999c71
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-10-04 20:07:01 +0900

    Update dendrogram.component.ts

--- study/src/app/dendrogram/dendrogram.component.ts
+++ study/src/app/dendrogram/dendrogram.component.ts
@@ -36,15 +36,16 @@ export class DendrogramComponent implements OnInit {
       cluster(root);
       console.log('root: ', root);
       console.log('root.descendants: ', root.descendants()); //배열 형태
+      console.log('data',data);
 
       // path
       svg.selectAll('path')
         .data(root.descendants().slice(1))
         .join('path')
-        // .attr("d", (d:any) => `M${d.y},${d.x}L${d.parent.y},${d.parent.x}`)
+        .attr("d", (d:any) => `M${d.y},${d.x}L${d.parent.y},${d.parent.x}`)
         // .attr("d", (d:any) => `M${d.y},${d.x}S${d.parent.y+100},${d.parent.x+100} ${d.parent.y},${d.parent.x}`)
         // .attr("d", (d:any) => `M${d.y},${d.x}Q${d.y+100},${d.x+100} ${d.parent.y},${d.parent.x}`)
-        .attr("d", (d:any) => `M${d.y},${d.x}C${d.parent.y+50},${d.x} ${d.parent.y+150},${d.parent.x} ${d.parent.y},${d.parent.x}`)
+        // .attr("d", (d:any) => `M${d.y},${d.x}C${d.parent.y+50},${d.x} ${d.parent.y+150},${d.parent.x} ${d.parent.y},${d.parent.x}`)
         .style("fill", 'none')
         .attr("stroke", '#ccc');
         
@@ -53,24 +54,30 @@ export class DendrogramComponent implements OnInit {
         .data(root.descendants())
         .join("g")
         .attr("transform", (d:any) => `translate(${d.y},${d.x})`)
+        // .attr("id",`tooltip${get_id()}`) //tooltip
         .append("circle")
         .attr("r", 7)
         .style("fill", "#69b3a2")
         .attr("stroke", "black")
-        .style("stroke-width", 2)
-        // .attr("id","node") //tooltip
+        .style("stroke-width", 2);
+        
+        
 
       // //tooltip
       // let tooltip=d3.select("#my_dataviz")
       //   .append("div")
       //   .style("position","absolute")
       //   .style("visibility","hidden")
-      //   .text("circle!!")
+      //   .text(`data:${get_id()}`);
 
-      // d3.select("#node")
-      //   .on("mouseover", () => tooltip.style("visibility", "visible"))
+      // d3.selectAll(`#tooltip${get_id()}`)
+      //   .on("mouseover", () => (tooltip.style("visibility", "visible"), console.log('mouse over')))
       //   .on("mousemove", (event:any) => tooltip.style("top", (event.pageY) + "px").style("left", (event.pageX) + "px"))
       //   .on("mouseout", () => tooltip.style("visibility", "hidden"));
+
+      // get_id() : Number{
+      //   return 1;
+      // }
     });
   }
 }
